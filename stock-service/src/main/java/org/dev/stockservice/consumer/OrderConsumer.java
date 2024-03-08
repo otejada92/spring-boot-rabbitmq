ee45d69735f5f695e0f25b6de3d4cb66c3df2d0e
Osvaldo Tejada <osvaldo.tl.92@gmail.com>
2024-03-08 13:21:14 -0400

    configure queue,binding,exchange, consumer and DTOs

--- stock-service/src/main/java/org/dev/stockservice/consumer/OrderConsumer.java
+++ stock-service/src/main/java/org/dev/stockservice/consumer/OrderConsumer.java
@@ -1,2 +1,17 @@
-package org.dev.stockservice.consumer;public class OrderConsumer {
+package org.dev.stockservice.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dev.stockservice.dto.OrderEvent;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderConsumer {
+
+    @RabbitListener(queues = {"${rabbitmq.queue.order.name}"})
+    public void consume(OrderEvent orderEvent) {
+
+        log.info("Order event recieved => {}", orderEvent.toString());
+    }
 }
