a70d85c378b54bf14eeb3bef0c00c4d457c9123e
Osvaldo Tejada <osvaldo.tl.92@gmail.com>
2024-03-08 11:45:09 -0400

    create producer and data, dto classes

---
+++ order-sevice/src/main/java/org/dev/ordersevices/publisher/OrderProducer.java
@@ -0,0 +1,29 @@
+package org.dev.ordersevices.publisher;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dev.ordersevices.dto.OrderEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String routingKey;
+
+    private final RabbitTemplate template;
+
+    public OrderProducer(RabbitTemplate template) {
+        this.template = template;
+    }
+
+    public void sendOrder(OrderEvent orderEvent) {
+        log.info("Order event sent to RabbitMQ => {}", orderEvent.toString());
+        template.convertAndSend(exchangeName, routingKey, orderEvent);
+    }
+}
