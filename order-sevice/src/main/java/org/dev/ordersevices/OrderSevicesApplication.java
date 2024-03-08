bb137d7a1ea55f21b0e4cd5779b6a4abb0653d88
Osvaldo Tejada <osvaldo.tl.92@gmail.com>
2024-03-08 11:08:21 -0400

    add email, order, stock services

---
+++ order-sevice/src/main/java/org/dev/ordersevices/OrderSevicesApplication.java
@@ -0,0 +1,13 @@
+package org.dev.ordersevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSevicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSevicesApplication.class, args);
+    }
+
+}
---
+++ order-sevice/src/test/java/org/dev/ordersevices/OrderSevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.ordersevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSevicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
